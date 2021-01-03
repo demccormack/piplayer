eacbc7d1defe67be6c28dc9cdc412627769c8595
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2021-01-04 11:17:20 +1300

    load first video immediately

--- piplayer.js
+++ piplayer.js
@@ -11,6 +11,7 @@ function getFilms() {
                 ihtml += newline;
             }
             document.getElementById("cb").innerHTML = ihtml;
+            selectFilm();
         }
     };
     xhttp.open("GET", "api", true);
