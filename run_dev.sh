a5a4b5d54e711fdf8fc3f2e0542b1df5c3958814
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2022-06-22 14:22:33 +1200

    Moved run_dev.sh and added usage info

--- run_dev.sh
+++ run_dev.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 
 export MEDIA="${1:-}"
+if [[ -z "$MEDIA" ]]
+then
+    echo "Usage: $0 /path/to/media/dir"
+    echo "As you have not defined the media directory, it will default to /var/www/html/media"
+fi
 
 docker build -t piplayer.backend -f ./docker/backend/Dockerfile . &
 docker build -t piplayer.frontend -f ./docker/frontend/Dockerfile . &
