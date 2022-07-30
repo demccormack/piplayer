0930b331a36fe95046109eb49f9e38d11bbc64eb
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2022-07-30 23:03:05 +1200

    Got tree working. Added documentation.

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -5,7 +5,34 @@ import {fetchApi} from './api.js';
 function Navbar(props) {
   const [items, setItems] = useState([]);
 
-  const populateTree = (items) => {
+  /**
+   * Returns the item from the hierarchy with the given url
+   * @param {Array} items
+   * @param {String} url
+   * @return {Object} obj
+  */
+  const getItemFromItems = (items, url) => {
+    let obj = [...items];
+    let dirs = url.split('/');
+    for ( // Skip the first part of the URL as it is not a directory
+      let i = 1; i < dirs.length; i++) {
+        /* This workaround is needed because the root of the state object has a different structure to 
+          the objects under it. To fix this properly, change the state object so the root also has children. */
+        if (i === 1) {
+          obj = obj.find(item => item.url.split('/')[i] === dirs[i]);
+        } else {
+          obj = obj.children.find(item => item.url.split('/')[i] === dirs[i]);
+        }
+    }
+    return obj;
+  }
+
+  /**
+   * Inserts the items returned by the API into the correct position in the state.
+   * @param {Array} items The items returned by the API.
+   * @param {String} url The url of the directory to populate.
+  */
+  const populateTree = (items, url) => {
     const result = items.map((item) => {
       return (item.type === 'directory') ? 
         {
@@ -15,17 +42,34 @@ function Navbar(props) {
         } : 
         item;
     });
-    setItems(oldItems => result);
+    setItems(oldItems => {
+      if (url) {
+        // Find the item with the url and populate its children
+        let newItems = [...oldItems];
+        let obj = getItemFromItems(newItems, url);
+        obj.children = result;
+        return newItems;
+      } else {
+        // Populate the root
+        return result;
+      }
+    });
   }
 
+  /**
+   * Expands or collapses a directory and gets its children if necessary.
+   * @param {String} url The url of the directory to expand or collapse. 
+   * This is the id of the checkbox.
+  */
   const handleCbCheckedChange = (url) => {
     setItems(oldItems => {
-      const result = oldItems.map((item) => {
-        if (item.url === url) {
-          item.expand = document.getElementById(url).checked;
-        }
-        return item;
-      });
+      const checked = document.getElementById(url).checked;
+      let result = [...oldItems];
+      let obj = getItemFromItems(result, url);
+      obj.expand = checked;
+      if (checked && obj.children.length === 0) {
+        fetchApi(url, populateTree);
+      }
       return result;
     })
   }
@@ -47,11 +91,12 @@ function Navbar(props) {
 
 
 function Dir(props) {
+  const {name, type, url, expand, children} = props.item;
   return (
     <div className='navItem'>
-      <input type='checkbox'  id={props.item.url} onChange={() => props.onChange(props.item.url)} />
-      <label className='dirLabel'>{props.item.name + props.item.expand}</label>
-      {props.item.expand && props.item.children.map(child => (
+      <input type='checkbox'  id={url} onChange={() => props.onChange(url)} />
+      <label className='dirLabel'>{name}</label>
+      {expand && children.map(child => (
         <Dir key={child.url} item={child} onChange={props.onChange} />
       ))}
     </div>
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,15 @@
 const API_ROOT = "http://127.0.0.1:8080/?dir=";
 
+/**
+ * Fetches a directory listing from the API and calls the callback function with the result.
+ * @param {String} url The url of the directory to fetch.
+ * @param {Function} callback The callback function to call with the result.
+*/
 export function fetchApi(url, callback) {
     fetch(`${API_ROOT}${url.slice(6)}`)
     .then(response => response.json())
     .then(items => {
       // Update the state with the list of items.
-      callback(items);
+      callback(items, url);
     });
   }
