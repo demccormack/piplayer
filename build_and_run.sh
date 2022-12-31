609733e8a2caf1822d7042e92ff79dde00ef316d
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2022-12-31 17:45:40 +1300

    Use the same script for development and production
    This works by:
     - sourcing the relevant .env file for the environment
     - using a different Dockerfile for the frontend

---
+++ build_and_run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -exuo pipefail
+
+ENV="$1"
+eval $(cat "$ENV" | sed -e '/^$/d' -e '/^#/d' -e 's/^/export /')
+
+build() {
+  if ! docker build -t piplayer."$1" -f ./docker/"$1"/Dockerfile .
+  then
+    echo "Failed to build $1" 1>&2
+    exit 1
+  fi
+}
+image_of() {
+  docker inspect $(docker ps | grep "$1" | awk '{print $1}') | grep Image | grep sha256
+}
+
+if [[ "$(docker ps | grep piplayer)" ]]
+then
+  PROXY_WAS=$(image_of proxy)
+  BACKEND_WAS=$(image_of backend)
+  FRONTEND_WAS=$(image_of $FRONTEND_IMAGE)
+fi
+
+build proxy
+build backend
+build $FRONTEND_IMAGE
+
+! [[ "$(docker ps | grep piplayer)" ]] || docker-compose --env-file $ENV down
+
+docker-compose --env-file="$ENV" ${DOCKER_COMPOSE_FILES:-} up --force-recreate -d
+
+docker ps
+
+# Clear out old unused images
+IMAGES=$(docker images | grep '<none>' | awk '{print $3}') || true
+! [[ "$IMAGES" ]] || docker rmi $IMAGES
+
+[[ "${PROXY_WAS:-}" == "$(image_of proxy)" ]] && echo "Proxy unchanged" || echo "Proxy has new image"
+[[ "${BACKEND_WAS:-}" == "$(image_of backend)" ]] && echo "Backend unchanged" || echo "Backend has new image"
+[[ "${FRONTEND_WAS:-}" == "$(image_of $FRONTEND_IMAGE)" ]] && echo "Frontend unchanged" || echo "Frontend has new image"
+echo "Done"
