52294d59148a5ff9b8dfb710be1f184901d296e8
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-02-11 17:40:30 +1300

    Delete frontend and replace with vite starter
    - React + TypeScript + SWC template
    - Customised to run dev server on port 3000

--- frontend/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-it('renders a video', () => {
-  render(<App />);
-  const video = screen.getByText('video');
-  expect(video).toBeInTheDocument();
-});
