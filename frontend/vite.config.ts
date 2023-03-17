0898144960ee8b54b3d5e8f40d3a58620e015d3c
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-03-18 00:15:09 +1300

    Setup vitest and React Testing Library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+    includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
+  },
   server: {
     port: 3000,
     strictPort: true,
