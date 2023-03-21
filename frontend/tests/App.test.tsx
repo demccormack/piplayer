42982b8347f8abecd5fc92f70424832b106d4e93
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-03-21 22:06:31 +1300

    Import jest-dom in setup file
    This seems to have made TypeScript/ESLint happy.

--- frontend/tests/App.test.tsx
+++ frontend/tests/App.test.tsx
@@ -1,6 +1,5 @@
 import App from '../src/App';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 it('renders', () => {
   const { container } = render(<App />);
--- frontend/vitest.setup.ts
+++ frontend/vitest.setup.ts
@@ -1,4 +1,5 @@
 import { expect } from 'vitest';
+import '@testing-library/jest-dom';
 import matchers from '@testing-library/jest-dom/matchers';
 
 // extends Vitest's expect method with methods from react-testing-library
