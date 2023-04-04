6b7d88c2bac7e4ee0ea6e387f7ae7fdf6a905de5
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-04-04 21:33:52 +1200

    Setup basic styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,17 @@
 function App() {
-  return <p className="text-3xl font-bold underline">Hello world!</p>;
+  return (
+    <div className="h-screen bg-gray-900 text-gray-400">
+      <MainPanel />
+    </div>
+  );
+}
+
+function MainPanel() {
+  return (
+    <div className="text-center">
+      <h1 className="p-5 text-4xl font-bold">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
