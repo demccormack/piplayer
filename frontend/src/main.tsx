19d1f7f211f10f755fb1ce1a1e10ba40cde7ef9a
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-04-22 21:48:29 +1200

    Add react-query dev tools

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 
@@ -10,6 +11,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="bottom-right"
+      />
     </QueryClientProvider>
   </React.StrictMode>,
 );
