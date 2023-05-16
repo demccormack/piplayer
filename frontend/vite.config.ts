5c3c230888d9d780fc85d7e5cb64819a2d4728e6
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-05-16 21:29:35 +1200

    Revert "Change to happy-dom"
    This reverts commit b244e6afaae741b4a8692aca74e996a4c76d33df because
    happy-dom doesn't support CSS pseudo classes like ::before. After
    switching back to jsdom my tests pass!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'happy-dom',
+    environment: 'jsdom',
     setupFiles: './vitest.setup.ts',
     includeSource: ['src/**/*.{js,ts,jsx,tsx}'],
   },
