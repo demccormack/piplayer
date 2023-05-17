0ae0cfbde9cda1e69871768bda4904ed9eb731ea
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-05-17 23:45:36 +1200

    Return realistic errors from mock query

--- frontend/tests/mocks/index.ts
+++ frontend/tests/mocks/index.ts
@@ -11,16 +11,83 @@ const queryFn: (
 ) => Promise<{ data: MediaItem[] | string }> = async (
   _API_ROOT,
   { params: { dir } },
-) =>
-  await Promise.resolve({
-    data:
-      dir === ''
-        ? top
-        : dir === 'Films'
-        ? Films
-        : dir === 'Films/OfficeSpace'
-        ? OfficeSpace
-        : '<!doctype html>\n<html lang=en>\n<title>500 Internal Server Error</title>\n<h1>Internal Server Error</h1>\n<p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>\n',
-  });
+) => {
+  const data =
+    dir === ''
+      ? top
+      : dir === 'Films'
+      ? Films
+      : dir === 'Films/OfficeSpace'
+      ? OfficeSpace
+      : undefined;
+
+  return data
+    ? await Promise.resolve({ data })
+    : await Promise.reject({
+        message: 'Request failed with status code 500',
+        name: 'AxiosError',
+        stack:
+          'AxiosError: Request failed with status code 500\n    at settle (http://localhost:3000/node_modules/.vite/deps/axios.js?v=429e27c8:1185:12)\n    at XMLHttpRequest.onloadend (http://localhost:3000/node_modules/.vite/deps/axios.js?v=429e27c8:1409:7)',
+        config: {
+          transitional: {
+            silentJSONParsing: true,
+            forcedJSONParsing: true,
+            clarifyTimeoutError: false,
+          },
+          adapter: ['xhr', 'http'],
+          transformRequest: [null],
+          transformResponse: [null],
+          timeout: 0,
+          xsrfCookieName: 'XSRF-TOKEN',
+          xsrfHeaderName: 'X-XSRF-TOKEN',
+          maxContentLength: -1,
+          maxBodyLength: -1,
+          env: {},
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+          },
+          params: {
+            dir,
+          },
+          method: 'get',
+          url: 'http://127.0.0.1:8080/',
+        },
+        code: 'ERR_BAD_RESPONSE',
+        status: 500,
+        response: {
+          data: '<!doctype html>\n<html lang=en>\n<title>500 Internal Server Error</title>\n<h1>Internal Server Error</h1>\n<p>The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.</p>\n',
+          status: 500,
+          statusText: 'INTERNAL SERVER ERROR',
+          headers: {
+            'content-length': '265',
+            'content-type': 'text/html; charset=utf-8',
+          },
+          config: {
+            transitional: {
+              silentJSONParsing: true,
+              forcedJSONParsing: true,
+              clarifyTimeoutError: false,
+            },
+            adapter: ['xhr', 'http'],
+            transformRequest: [null],
+            transformResponse: [null],
+            timeout: 0,
+            xsrfCookieName: 'XSRF-TOKEN',
+            xsrfHeaderName: 'X-XSRF-TOKEN',
+            maxContentLength: -1,
+            maxBodyLength: -1,
+            env: {},
+            headers: {
+              Accept: 'application/json, text/plain, */*',
+            },
+            params: {
+              dir,
+            },
+            method: 'get',
+            url: 'http://127.0.0.1:8080/',
+          },
+        },
+      });
+};
 
 export { queryFn };
