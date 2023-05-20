b26e0fb573cd347ae354d9734394d152a208fab1
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-05-20 21:53:26 +1200

    Allow passing custom query function for each test

--- frontend/tests/utils/index.tsx
+++ frontend/tests/utils/index.tsx
@@ -1,25 +1,37 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MediaItem, QueryContext } from '../../src/App';
+import { MediaItem, QueryContext, queryFnType } from '../../src/App';
 import { ReactElement } from 'react';
 import { RenderOptions, render } from '@testing-library/react';
-import { queryFn } from '../mocks';
+import { queryFn as mockQueryFn } from '../mocks';
 
 const randomItemNameFrom = (array: MediaItem[]) =>
   array[Math.floor(Math.random() * array.length)].name;
 
 const queryClient = new QueryClient();
 
-const wrapper = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <QueryContext.Provider value={{ queryFn }}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </QueryContext.Provider>
-  );
-};
+const wrapperWith =
+  (queryFn: queryFnType) =>
+  ({ children }: { children: React.ReactNode }) => {
+    return (
+      <QueryContext.Provider value={{ queryFn }}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </QueryContext.Provider>
+    );
+  };
 
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, 'wrapper'>,
-) => render(ui, { wrapper, ...options });
+  {
+    queryFn,
+    ...options
+  }: Omit<RenderOptions, 'wrapper'> & { queryFn: queryFnType } = {
+    queryFn: mockQueryFn,
+  },
+) => {
+  const wrapper = wrapperWith(queryFn);
+  return render(ui, { wrapper, ...options });
+};
 
 export { customRender as render, randomItemNameFrom };
