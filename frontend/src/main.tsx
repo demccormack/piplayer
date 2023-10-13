004591bb79f0ed4c82e018d720e95b497a603a54
Daniel McCormack <52194107+demccormack@users.noreply.github.com>
2023-10-13 22:17:00 +1300

    Use msw in tests

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App, { queryFn, QueryContext } from './App';
+import App from './App';
 import './index.css';
-import { worker } from '../tests/mocks/browser';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -14,16 +13,10 @@ const queryClient = new QueryClient({
   },
 });
 
-if (process.env.NODE_ENV === 'development') {
-  void worker.start();
-}
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <QueryContext.Provider value={{ queryFn }}>
-        <App />
-      </QueryContext.Provider>
+      <App />
       <ReactQueryDevtools
         initialIsOpen={false}
         position="bottom-right"
--- frontend/tests/utils/index.tsx
+++ frontend/tests/utils/index.tsx
@@ -1,35 +1,24 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { MediaItem, QueryContext, queryFnType } from '../../src/App';
+import { MediaItem } from '../../src/App';
 import { ReactElement } from 'react';
 import { RenderOptions, render } from '@testing-library/react';
-import { queryFn as mockQueryFn } from '../mocks';
 
 const randomItemNameFrom = (array: MediaItem[]) =>
   array[Math.floor(Math.random() * array.length)].name;
 
-const wrapperWith =
-  (queryFn: queryFnType) =>
-  ({ children }: { children: React.ReactNode }) => {
-    return (
-      <QueryContext.Provider value={{ queryFn }}>
-        <QueryClientProvider client={new QueryClient()}>
-          {children}
-        </QueryClientProvider>
-      </QueryContext.Provider>
-    );
-  };
+const wrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      {children}
+    </QueryClientProvider>
+  );
+};
 
 const customRender = (
   ui: ReactElement,
-  {
-    queryFn,
-    ...options
-  }: Omit<RenderOptions, 'wrapper'> & { queryFn: queryFnType } = {
-    queryFn: mockQueryFn,
-  },
+  { ...options }: Omit<RenderOptions, 'wrapper'> = {},
 ) => {
   expect(document.body).toBeEmptyDOMElement();
-  const wrapper = wrapperWith(queryFn);
   return render(ui, { wrapper, ...options });
 };
 
